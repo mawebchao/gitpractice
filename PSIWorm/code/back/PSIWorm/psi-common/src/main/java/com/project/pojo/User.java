c1bcf130485ac7e83972dab800bb805f9451dbd7
mawebchao <13801688583@163.com>
2021-10-30 21:25:25 +0800

    update

--- PSIWorm/code/back/PSIWorm/psi-common/src/main/java/com/project/pojo/User.java
+++ PSIWorm/code/back/PSIWorm/psi-common/src/main/java/com/project/pojo/User.java
@@ -1,15 +1,21 @@
 package com.project.pojo;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
+@TableName("user")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Accessors(chain = true)
 public class User {
+    @TableId(type = IdType.AUTO)
+    Integer id;
     String username;
     String password;
 }
---
+++ PSIWorm/code/back/PSIWorm/psi-user/src/main/java/com/project/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.project.pojo.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
